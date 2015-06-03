77806ebe151a0bf38778af2ee3c0acc7fdca3cc2
saw111 <saw111@pitt.edu>
2015-06-03 12:57:11 -0400

    Refactored.

--- test/integrations/server/TestLandingPage.java
+++ test/integrations/server/TestLandingPage.java
@@ -14,12 +14,16 @@ public class TestLandingPage {
     @Test
     public void landingPageShown() {
         Callback<TestBrowser> block = browser -> {
-        	ConfRule conf = Factory.makeConfRule();
-        	String context = conf.readString("application.context");
+        	String context = readContext();
             browser.goTo("http://localhost:3333" + context);
             String expected = "Your new application is ready.";
 			assertThat(browser.pageSource()).contains(expected);
         };
 		running(testServer(3333), HTMLUNIT, block);
     }
+
+	private String readContext() {
+		ConfRule conf = Factory.makeConfRule();
+		return conf.readString("application.context");
+	}
 }
