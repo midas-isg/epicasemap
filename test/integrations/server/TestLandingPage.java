e6ade920a747d66f95035b7188d53450cd7ff575
saw111 <saw111@pitt.edu>
2015-06-05 10:46:45 -0400

    TestLandingPage containsConextForJavaScript.

--- test/integrations/server/TestLandingPage.java
+++ test/integrations/server/TestLandingPage.java
@@ -1,4 +1,5 @@
 package integrations.server;
+
 import static org.fest.assertions.Assertions.assertThat;
 import static play.test.Helpers.HTMLUNIT;
 import static play.test.Helpers.running;
@@ -10,17 +11,30 @@ import org.junit.Test;
 
 import play.libs.F.Callback;
 import play.test.TestBrowser;
+
 public class TestLandingPage {
     @Test
-    public void landingPageShown() {
+    public void containsConextForJavaScript() {
         Callback<TestBrowser> block = browser -> {
         	String context = readContext();
-            browser.goTo("http://localhost:3333" + context);
-            String expected = "Your new application is ready.";
-			assertThat(browser.pageSource()).contains(expected);
+            String url = "http://localhost:3333" + context;
+            String actual = pageSource(browser, url);
+			String expected = javaScriptContext(context);
+			assertThat(actual).contains(expected);
         };
 		running(testServer(3333), HTMLUNIT, block);
     }
+
+	private String javaScriptContext(String context) {
+		String template = "var CONTEXT = '%s';";
+		String expected = String.format(template, context);
+		return expected;
+	}
+
+	private String pageSource(TestBrowser browser, String url) {
+		browser.goTo(url);
+		return browser.pageSource();
+	}
 
 	private String readContext() {
 		ConfRule conf = Factory.makeConfRule();
