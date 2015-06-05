3f28f44a9e544b99361f653e5b1052eb57abdc1a
saw111 <saw111@pitt.edu>
2015-06-05 10:21:05 -0400

    Removed TestIndexPage

--- test/unit/UnitTests.java
+++ test/unit/UnitTests.java
@@ -6,10 +6,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import views.TestIndexPage;
-
 @RunWith(Suite.class)
-@SuiteClasses({TestConfRule.class, TestIndexPage.class})
+@SuiteClasses({TestConfRule.class})
 public class UnitTests {
 
 }
