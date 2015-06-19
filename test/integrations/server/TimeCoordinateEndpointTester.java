42c2fec54ed5f81af9a31e55de077d9ba2aa7759
saw111 <saw111@pitt.edu>
2015-06-19 18:23:51 -0400

    Assersted fields exhaustively

--- test/integrations/server/TimeCoordinateEndpointTester.java
+++ test/integrations/server/TimeCoordinateEndpointTester.java
@@ -6,6 +6,7 @@ import static java.time.Instant.EPOCH;
 import static org.fest.assertions.Assertions.assertThat;
 import static suites.Helper.assertAreEqual;
 import static suites.Helper.assertNodeType;
+import static suites.Helper.assertValueRange;
 import static suites.Helper.testJsonResponseLimit;
 import static suites.Helper.testJsonResponseMin;
 
@@ -58,11 +59,24 @@ public class TimeCoordinateEndpointTester {
 
 	private void assertCoordinates(JsonNode results) {
 		assertThat(results).isNotEmpty();
-		JsonNode node = results.get(0);
+		assertFields(results.get(0));
+	}
+
+	private void assertFields(JsonNode node) {
 		Iterator<String> fields = node.fieldNames();
-		String idName = "id";
-		assertThat(fields).containsOnly(idName, "seriesId", "value", "timestamp", "latitude", "longitude");
-		assertThat(node.get(idName).asLong()).isPositive();
+		final String id = "id";
+		final String sid = "seriesId";
+		final String val = "value";
+		final String ts = "timestamp";
+		final String lat = "latitude";
+		final String lon = "longitude";
+		assertThat(fields).containsOnly(id, sid, val, ts, lat, lon);
+		assertThat(node.get(id).asLong()).isPositive();
+		assertThat(node.get(sid).asLong()).isPositive();
+		assertThat(node.get(val).asDouble()).isPositive();
+		assertThat(node.get(ts).asLong()).isPositive();
+		assertValueRange(node.get(lat), 90.0, -90.0);
+		assertValueRange(node.get(lon), 180.0, -180.0);
 	}
 
 	private void assertDefaultFilter(JsonNode filter) {
