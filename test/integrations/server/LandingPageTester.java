c15a89002327882b18f7a7cb853bd86a3300cfc2
saw111 <saw111@pitt.edu>
2015-06-19 16:10:55 -0400

    Renamed test classes.

--- test/integrations/server/LandingPageTester.java
+++ test/integrations/server/LandingPageTester.java
@@ -3,11 +3,11 @@ package integrations.server;
 import static org.fest.assertions.Assertions.assertThat;
 import suites.Helper;
 
-public class TestLandingPage {
+public class LandingPageTester {
 	public static Runnable containsConextForJavaScript() {
         Runnable block = () -> {
         	String context = Server.getContext();
-        	String expected = javaScriptContext(context);
+        	String expected = new LandingPageTester().javaScriptContext(context);
         	String url = Server.makeTestUrl("");
             String actual = Helper.get(url).getBody();
 			assertThat(actual).contains(expected);
@@ -15,7 +15,7 @@ public class TestLandingPage {
 		return block;
     }
 
-	private static String javaScriptContext(String context) {
+	private String javaScriptContext(String context) {
 		String template = "var CONTEXT = '%s';";
 		String expected = String.format(template, context);
 		return expected;
