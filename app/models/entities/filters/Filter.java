6d2dae057f304d8efd3ba04ca7e2366959e92d89
saw111 <saw111@pitt.edu>
2015-06-23 16:44:22 -0400

    Made time-coordinate ordered by timestamp asc.

--- app/models/entities/filters/Filter.java
+++ app/models/entities/filters/Filter.java
@@ -1,7 +1,16 @@
 package models.entities.filters;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public interface Filter {
+
+	public enum Order{
+		ASC,
+		DESC
+	};
+	
 	public abstract Map<String, Object> getEqualities();
+
+	public abstract LinkedHashMap<String, Order> getOrder();
 }
