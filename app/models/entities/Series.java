0c83af79c34e74a3aba3a8ed3e44774499c9fd24
saw111 <saw111@pitt.edu>
2015-06-26 14:26:16 -0400

    Made persistNewSeries() public and repeatable.

--- app/models/entities/Series.java
+++ app/models/entities/Series.java
@@ -1,11 +1,13 @@
 package models.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Series {
-	@Id
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String name;
 	private String description;
