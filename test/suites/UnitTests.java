70bf4e492dae7114f96e622526d6c2b951ef8e47
saw111 <saw111@pitt.edu>
2015-07-08 11:37:01 -0400

    Removed TestCoordinate.

--- test/suites/UnitTests.java
+++ test/suites/UnitTests.java
@@ -3,7 +3,6 @@ package suites;
 import gateways.database.TestCoordinateDao;
 import interactors.TestConfRule;
 import interactors.TestVizRule;
-import models.entities.TestCoordinate;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -12,7 +11,6 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
 	TestCoordinateDao.class,
-	TestCoordinate.class,
 	TestConfRule.class,
 	TestVizRule.class,
 	CoverageBooster.class
