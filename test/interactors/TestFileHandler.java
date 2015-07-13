03ca636080e01741d0f10b70c03ae3181503ae18
Amin <amin.tajgardoon@gmail.com>
2015-07-13 12:57:46 -0400

    test unit modified

--- test/interactors/TestFileHandler.java
+++ test/interactors/TestFileHandler.java
@@ -34,23 +34,8 @@ public class TestFileHandler {
 	
 
 	private  DelimitedFile createTestFileWithApolloIDformat() {
-		String fileContent = "time,apollo ID,value\n"
-				+ "2015-01-01,1,1\n"
-				+ "2015-01-01T10:15,2,2\n"
-				+ "2015-02-01T10:15:12,3,3\n"
-				+ "2015-02-01T10:15:12+05,4,4\n"
-				+ "2015-02-01T10:15:12+05:00,5,5\n";
-		
-		File csvFile = new File("test/resources/test_input.txt");
-		BufferedWriter  bw = null;
-		try {
-			bw = new BufferedWriter(new FileWriter(csvFile));
-			bw.write(fileContent);
-			bw.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
+
+		File csvFile = new File("test/resources/test_input.txt");	
 		Map<String,String[]> metaData = new HashMap<String,String[]>();
 		metaData.put("title",new String[]{"serie1"});
 		metaData.put("format",new String[]{"apolloIdFormat"});
