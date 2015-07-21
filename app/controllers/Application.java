836d3f6514c0c25645c5f66d90e97777192cf76f
saw111 <saw111@pitt.edu>
2015-07-21 14:15:09 -0400

    Added App info as footer into main.scala.html.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,15 +1,32 @@
 package controllers;
 
+import gateways.configuration.AppKey;
+import interactors.ConfRule;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class Application extends Controller {
+    private static String appVersion = "unknown Version";
+   	private static String dbName = "unknown Database";
+   	
+   	static {
+   		final ConfRule confRule = Factory.makeConfRule();
+   		appVersion = "Version: " + confRule.readString(AppKey.VERSION.key());
+   		dbName = "Database: " + confRule.readString("db.default.url");
+   	}
+   	
+
     public static Result index() {
         return ok(views.html.index.render("Your new application is ready."));
     }
     
     public static Result manageVizs() {
         return ok(views.html.vizs.render());
+    }
+
+    public static String info() {
+        return "Copyright 2015 - University of Pittsburgh, " 
+        		+ appVersion + ", " + dbName;
     }
 
     public static Result swagger() {
