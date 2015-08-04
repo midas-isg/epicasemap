d38a2ff93e2ae9575bed228dc2dd1f8d5c0f5167
saw111 <saw111@pitt.edu>
2015-08-04 11:04:11 -0400

    Used Angular model in series_data_dialog.

--- public/javascripts/ng/controllers/series-data.js
+++ public/javascripts/ng/controllers/series-data.js
@@ -1,41 +1,50 @@
-"use strict"
+"use strict";
 
 app.controller('SeriesData', function($scope, $rootScope, api) {
+	var dom = {};
+	cacheDOM();
+	bindEvents();
 	$scope.view = {};
-	$scope.dialog = $('#dataModal');
-    $scope.dialog.on('shown.bs.modal', function (e) {
-    	$scope.dialog.find('form').find(':input:enabled:visible:first').focus();
-    });
-
-	$scope.closeDialog = function() {
-		$scope.dialog.modal('hide');
+	$scope.closeDialog = function() { 
+		dom.$dialog.modal('hide'); 
 	};
-    $rootScope.$on('uploadNewSeriesData', function(event, seriesId) {
-    	showDialog(seriesId);
-	});
-    $scope.uploadThenClose = function() {
-    	uploadThenClose();
-	};
+    $scope.uploadThenClose = uploadThenClose;
 	
-	function showDialog(seriesId){
-		$scope.seriesId = seriesId;
-		$scope.dialog.modal();
+	function cacheDOM(){
+		dom.$dialog = $('#dataModal');
+		dom.$form = dom.$dialog.find('form');
+	}
+	
+	function bindEvents(){
+	    $rootScope.$on('uploadNewSeriesData', showDialog);
+	    dom.$dialog.on('shown.bs.modal', focusFirstFormInput);
+
+		function showDialog(event, seriesId){
+			$scope.seriesId = seriesId;
+			dom.$dialog.modal();
+		}
+		
+		function focusFirstFormInput(event) {
+	    	dom.$form.find(':input:enabled:visible:first').focus();
+	    }
 	}
 	
 	function uploadThenClose(){
-		var seriesId = $scope.seriesId;
-		var path = "fileUpload/" + seriesId + "/" + encodeURIComponent($("#delimiter").val()) + "/" + encodeURIComponent($("#format").val());
-        var file = $scope.dataFile;
-        api.uploadFile(path, file).then(function(rsp) {
+        api.uploadFile(makePath(), $scope.dataFile).then(function(rsp) {
 			$scope.closeDialog();
 			loadCoordinates($scope.seriesId);
 
 			$("#data-file").replaceWith($("#data-file").clone(true));
 			delete($scope.dataFile);
 		});
+
+        function makePath(){
+        	return "fileUpload/" + $scope.seriesId + 
+        	"/" + encodeURIComponent($scope.delimiter) + 
+        	"/" + encodeURIComponent($scope.format);
+        }
+        function loadCoordinates(seriesId) {
+        	$rootScope.$emit('loadCoordinates', seriesId);
+    	}
 	}
-	
-	function loadCoordinates(seriesId) {
-    	$rootScope.$emit('loadCoordinates', seriesId);
-	};
 });
