35da5384add9e7a4d6f73803c88d10e0f78ba3cf
saw111 <saw111@pitt.edu>
2015-10-29 09:37:50 -0400

    Fixed broken test cases and added more.

--- public/javascripts/spec/app.spec.js
+++ public/javascripts/spec/app.spec.js
@@ -1,24 +1,60 @@
 describe('Module: app: Security', () => {
 	'use strict'; `comment: use ES6`;
-	var newPublicVizId = null;
-	var newPublicSeriesId = null;
+	var my = {publicAccountId:1};
+	var seriesId2permissionId = {}
+	let testEmail = 'test@test.com';
 	
 	afterAll(() =>{
-		let vPath = toVizPath(newPublicVizId);
+		let vPath = toVizPath(my.publicVizId);
 		login(()=>{
-			put(vPath, {}, ()=>{
-				remove(vPath, ()=>console.log(`${vPath} deleted`));
+			put(vPath, {ownerId:my.publicAccountId}, ()=>{
+				del(vPath);
+				del(toSeriesPath(my.seriesIdSharingNone));
+				delTree(my.seriesIdSharingToPublic);
+				delTree(my.seriesIdSharingToTest);
 				
-				let path = toSeriesPath(newPublicSeriesId);
-				remove(path, ()=>console.log(`${path} deleted`));
+				function delTree(id){
+					remove(toPath(id), ()=>{
+						del(toSeriesPath(id));
+					}, nop);
+				}
+				function del(path){
+					remove(path, ()=>console.log(`${path} deleted`), nop);
+				}
+				function toPath(id){
+					return `api/series/permissions/${seriesId2permissionId[id]}`;
+				}
+				function nop(){}
 			});
 		});
 
 	});
 	
-	describe(`when logging in`, () => {
+	describe(`when logging in as Public`, () => {
 		beforeEach(done => {
-			login(done, done);
+			login(()=>{
+				if (! my.testAccountId){
+					getTestAccountId();
+				} else {
+					done();
+				}
+			}, done);
+		
+			function getTestAccountId(){
+				get('api/accounts', (body)=>{
+					let testAccount = _.find(body.results, (account) =>{
+						return account.email === testEmail;
+					});
+					if (testAccount){
+						my.testAccountId = testAccount.id;
+						done();
+					} else {
+						done.fail(`Cannot find the account with email = ${testEmail}. Please create the account with password='test' and rerun the tests`)
+					}
+				}, () => {
+		        	done.fail(`get accounts returned an error!`);
+				});
+			}
 		});
 		
 		testSeriesAfterLoggedIn();
@@ -33,42 +69,149 @@ describe('Module: app: Security', () => {
 		
 		testVizsAfterLoggedOut();
 	});
+	describe(`when logging in as Test`, () => {
+		beforeEach(done => {
+			loginAsTest(done, done);
+		});
+		
+		//testSeriesAfterLoggedInAsTest();
+		testVizAfterLoggedInAsTest();
+	});
+
+	function testVizAfterLoggedInAsTest(){
+		var vizId = null;
+		describe(`then creating new Viz`, () => {
+			beforeEach(done => {
+				let path = toVizPath();
+				let seriesIds = [my.seriesIdSharingToPublic, my.seriesIdSharingToTest];
+				post(path, {seriesIds}, (data, textStatus, rsp)=>{
+					vizId = toId(rsp, path); 
+					done();
+				}, done);
+			});
+			
+			it('should create a new Viz with a positive ID',()=>{
+				expect(vizId).toBeGreaterThan(0);
+			});
+		});
+		
+		describe(`then creating new Viz with unauthorized Series`, () => {
+			var statusMessage = null;
+			beforeEach(done => {
+				let path = toVizPath();
+				post(path, {seriesIds:[my.seriesIdSharingNone]}, ()=>{
+					done.fail(`shouldn't permit to create new Viz`);
+				}, (body, text, rsp)=>{
+					statusMessage = rsp;
+					done();
+				});
+			});
+			let errorMessage = 'Unauthorized'
+			it(`should return error ${errorMessage}`,()=>{
+				expect(statusMessage).toBe(errorMessage);
+			});
+		});
+
+		describe('then delete the new Viz', ()=>{
+			var vPath = null;
+			beforeEach(done => {
+				vPath = toVizPath(vizId);
+				put(vPath, {ownerId:my.testAccountId}, ()=>{
+					remove(vPath, done, done);
+				});
+			});
+			
+			it('should delete the new Viz', done=>{
+				get(vPath, ()=>{
+					done.fail(`shouldn't get deleted Viz id=${vizId}`);
+				}, (body, text, rsp) => {
+		        	expect(rsp).toBe('Not Found');
+		        	done();
+				});
+			});
+		});
+	}
 	
 	function login(success, done){
 		let body = {email:'public@test.com',password:'public'};
 		post('login', body, success, done);
 	}
 	
+	function loginAsTest(success, done){
+		let body = {email:'test@test.com',password:'test'};
+		post('login', body, success, done);
+	}
 	function testSeriesAfterLoggedIn(){
 		describe(`then creating new Series`, () => {
 			beforeEach(done => {
 				let path = toSeriesPath();
-				newPublicSeriesId = null;
+				my.seriesIdSharingNone = null;
 				post(path, {}, (data, textStatus, rsp)=>{
-					newPublicSeriesId = toId(rsp, path); 
+					my.seriesIdSharingNone = toId(rsp, path); 
 					done();
 				}, done);
 			});
 			
 			it('should create a new Series with a positive ID',()=>{
-				expect(newPublicSeriesId).toBeGreaterThan(0);
+				expect(my.seriesIdSharingNone).toBeGreaterThan(0);
 			});
 		});
+		
+		testShareNewSeriesToId('testAccountId', 'seriesIdSharingToTest');
+		testShareNewSeriesToId('publicAccountId', 'seriesIdSharingToPublic');
+		
+		
+		function testShareNewSeriesToId(aKey, sKey){
+		describe(`then creating new Series sharing to Test`, () => {
+			var id = null;
+			var permission = null;
+			var seriesPermissionsPath = null;
+			beforeEach(done => {
+				let path = toSeriesPath();
+				id = my[aKey];
+				post(path, {}, (data, textStatus, rsp)=>{
+					let idKey = toId(rsp, path);
+					my[sKey] = idKey; 
+					let accountIds = [id];
+					seriesPermissionsPath = toSeriesPath(my[sKey]) + '/permissions';
+					post(seriesPermissionsPath, {use: true, accountIds, idKey}, done, done);
+				}, done);
+			});
+			
+			it('should create a new Series with a positive ID with permission', (done)=>{
+				expect(my[sKey]).toBeGreaterThan(0);
+				get(seriesPermissionsPath, (body)=>{
+					expect(body.results.length).toBeGreaterThan(0);
+					let p = body.results[0];
+					let pId = p.id;
+					expect(pId).toBeGreaterThan(0);
+					expect(p.use).toBe(true);
+					expect(p.account.id).toBe(id);
+					let seriesId = p.series.id;
+					expect(seriesId).toBeGreaterThan(0);
+					seriesId2permissionId[seriesId] = pId;
+					done();
+				},() => {
+		        	done.fail(`logging out returned an error!`);
+				} )
+			});
+		});
+		}
 	}
 	
 	function testVizAfterLoggedIn(){
 		describe(`then creating new Viz`, () => {
 			beforeEach(done => {
 				let path = toVizPath();
-				newPublicVizId = null;
-				post(path, {seriesIds:[newPublicSeriesId]}, (data, textStatus, rsp)=>{
-					newPublicVizId = toId(rsp, path); 
+				my.publicVizId = null;
+				post(path, {seriesIds:[my.seriesIdSharingNone, my.seriesIdSharingToTest]}, (data, textStatus, rsp)=>{
+					my.publicVizId = toId(rsp, path); 
 					done();
 				}, done);
 			});
 			
 			it('should create a new Viz with a positive ID',()=>{
-				expect(newPublicVizId).toBeGreaterThan(0);
+				expect(my.publicVizId).toBeGreaterThan(0);
 			});
 		});
 	}
@@ -118,12 +261,15 @@ describe('Module: app: Security', () => {
 		let url = toUrl(path);
 		$.ajax({type, url, data, success, error});
 		
-		function error(){
+		function error(body, text, rsp){
 			let msg = `${type} ${url} with ${JSON.stringify(data)} returned an error!`;
 			if (! done){
 				fail(msg);
 			} else {
-				done.fail(msg);
+				if(done.fail)
+					done.fail(msg);
+				else
+					done(body, text, rsp);
 			}
 		}
 	}
@@ -134,7 +280,7 @@ describe('Module: app: Security', () => {
 		describe(`then getting Series data via a permitted Viz`, () => {
 			var response;
 			beforeEach(done => {
-				let path = toSeriesDataPath(newPublicVizId, newPublicSeriesId);
+				let path = toSeriesDataPath(my.publicVizId, my.seriesIdSharingNone);
 				get(path, data => {
 					response = data;
 					done();
@@ -149,7 +295,7 @@ describe('Module: app: Security', () => {
 		describe(`then getting Series data via an unauthorized Viz`, () => {
 			var error = {};
 			beforeEach(done => {
-				let path = toSeriesDataPath(unauthorizedVizId, newPublicSeriesId);
+				let path = toSeriesDataPath(unauthorizedVizId, my.seriesIdSharingNone);
 				error.result = getExpectingError(path, done);
 			});
 			assertErrorTypeWithUserMessage(error, 'Unauthorized');
@@ -158,7 +304,7 @@ describe('Module: app: Security', () => {
 			var error = {};
 			beforeEach(done =>{
 				let notInVizSeriesId = 19;
-				let path = toSeriesDataPath(newPublicVizId, notInVizSeriesId);
+				let path = toSeriesDataPath(my.publicVizId, notInVizSeriesId);
 				error.result = getExpectingError(path, done);
 			});
 			assertErrorTypeWithUserMessage(error, 'Not Found');
@@ -167,7 +313,7 @@ describe('Module: app: Security', () => {
 			var response;
 			var id;
 			beforeEach(done => {
-				id = newPublicVizId;
+				id = my.publicVizId;
 				let path = toVizPath(id);
 				get(path, data => {
 					response = data;
